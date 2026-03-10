bb3de633b60aa689f7da7ad9b61c1e02ec3dcfa3
gopal-git-18 <gopalshete057@gmail.com>
2026-03-10 08:42:30 +0530

    Initial commit

---
+++ src/main/java/com/localconnect/LocalconnectApplication.java
@@ -0,0 +1,14 @@
+package com.localconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalconnectApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(LocalconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localconnect/LocalconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
